596be33a1d0a514895b94c30443510c4f5c752f6
merhu <jlepler@montgomerycollege.edu>
2019-07-24 16:47:48 -0400

    last commit

---
+++ src/main/java/com/example/demo/UserService.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    RoleRepository roleRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+
+}
